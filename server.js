43075a523e5d0aa01eebc55afd7e5cc86944a0c8
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-03-10 22:10:12 -0600

    created a server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const apiRoutes = require("./routes/api/routes");
+const htmlRoutes = require("./routes/html-routes");
+
+const PORT = 3001;
+
+const app = express();
+
