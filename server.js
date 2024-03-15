8880daab03383c58a578bea5fcf599265339b60d
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-03-14 21:20:18 -0600

    added stuff

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const PORT = 3001;
 
 const app = express();
 
+app.use(express.urlencoded(( extended: false )));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(apiRoutes)
+
+app.use(htmlRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
