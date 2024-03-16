8883dbd8b52aa4012ac07a89dd39289916bc96e0
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-03-15 22:10:24 -0600

    getting things to work

--- server.js
+++ server.js
@@ -1,21 +1,68 @@
-const express = require("express");
-const apiRoutes = require("./routes/api/routes");
-const htmlRoutes = require("./routes/html-routes");
+// Dependencies
+const express = require('express');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
-const PORT = 3001;
-
+// Sets up the server
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded(( extended: false )));
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+// Route for index.html
+app.get('/', (req, res) => {
+    const indexPath = path.join(__dirname, './public/index.html');
+    res.sendFile(indexPath, (err) => {
+        if (err) {
+            console.error('Error sending index.html:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
 
-app.use(apiRoutes)
+// Routes for notes.html
+app.get('/notes', (req, res) => {
+    const indexPath = path.join(__dirname, './public/notes.html');
+    res.sendFile(indexPath, (err) => {
+        if (err) {
+            console.error('Error sending index.html:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
 
-app.use(htmlRoutes)
+// Post a new note
+app.post('/notes', (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    dbJson.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbJson));
+    res.json(newNote);
+});
+
+// API routes
+app.get('/api/notes', async (req, res) => {
+    const indexPath = path.join(__dirname, './public/notes.html');
+    const dbJson = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    const newFeedback = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    dbJson.push(newFeedback);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbJson));
+    res.json(dbJson);
+});
+
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
